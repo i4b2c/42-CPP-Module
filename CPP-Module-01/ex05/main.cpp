704f468232dd11ba45b8129c3604332299aecfd6
i4b2c <traiguuhh@gmail.com>
2023-11-25 18:44:23 +0100

    not started yet

---
+++ CPP-Module-01/ex05/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Harl
+{
+	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+	public:
+		void complain(std::string level);
+}
+
+
+
+int main(void)
+{
+
+}
