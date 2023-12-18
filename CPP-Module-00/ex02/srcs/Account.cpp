9046353763861ae3b1135ba25538329e4462ca75
i4b2c <traiguuhh@gmail.com>
2023-12-18 14:50:44 +0000

    update ex02

--- CPP-Module-00/ex02/srcs/Account.cpp
+++ CPP-Module-00/ex02/srcs/Account.cpp
@@ -20,16 +20,14 @@ Account::Account(int initial_deposit)
 	this->_nbWithdrawals = 0;
 	Account::_totalAmount += initial_deposit;
 	std::cout << "index:" << this->_accountIndex << ';'
-	<< "amount:" << this->_amount << ';'
-	<< "created" << std::endl;
+	<< "amount:" << this->_amount << ";" << "created" << std::endl;
 }
 
 Account::~Account(void)
 {
 	Account::_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ';'
-	<< "amount:" << this->_amount << ';'
-	<< "closed" << std::endl;
+	<< "amount:" << this->_amount << ";" << "closed" << std::endl;
 }
 
 int Account::getNbAccounts(void)
@@ -57,7 +55,7 @@ void Account::displayAccountsInfos(void)
 	Account::_displayTimestamp();
 	std::cout << "accounts:" << getNbAccounts() << ';'
 	<< "total:" << getTotalAmount() << ';'
-	<< "deposit:" << getNbDeposits() << ';'
+	<< "deposits:" << getNbDeposits() << ';'
 	<< "withdrawals:" << getNbWithdrawals() << std::endl;
 }
 
@@ -72,7 +70,7 @@ void Account::makeDeposit(int deposit)
 	<< "p_amount:" << this->_amount - deposit << ';' \
 	<< "deposit:" << deposit << ";" \
 	<< "amount:" << this->_amount << ";" \
-	<< "nb_deposits:" << this->_nbDeposits << ';' << std::endl;
+	<< "nb_deposits:" << this->_nbDeposits << std::endl;
 }
 
 bool Account::makeWithdrawal(int withdrawal)
@@ -93,7 +91,7 @@ bool Account::makeWithdrawal(int withdrawal)
 		Account::_totalNbWithdrawals += 1;
 		std::cout << "withdrawal:" << withdrawal << ";" \
 		<< "amount:" << this->_amount << ";" \
-		<< "nb_withdrawals:" << this->_nbWithdrawals << ';' << std::endl;
+		<< "nb_withdrawals:" << this->_nbWithdrawals << std::endl;
 	}
 	return check;
 }
@@ -109,7 +107,7 @@ void Account::displayStatus(void) const
 	std::cout << "index:" << this->_accountIndex << ';'
 	<< "amount:" << this->_amount << ';'
 	<< "deposits:" << this->_nbDeposits << ';'
-	<< "withdrawals:" << this->_nbWithdrawals << ';' << std::endl;
+	<< "withdrawals:" << this->_nbWithdrawals << std::endl;
 }
 
 void Account::_displayTimestamp(void)
