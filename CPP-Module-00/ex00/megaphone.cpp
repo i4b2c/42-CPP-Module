e0c903b3efd9819385a4789916002568d7d67524
icaldas <traiguuhh@gmail.com>
2024-01-29 16:33:44 +0000

    ex00 smaller

--- CPP-Module-00/ex00/megaphone.cpp
+++ CPP-Module-00/ex00/megaphone.cpp
@@ -2,29 +2,15 @@
 #include <cctype>
 #include <cstdlib>
 
-void ft_putstr(char *str)
-{
-	int i;
-	for(i = 0; str[i]; i++)
-		std::cout << char(toupper(str[i]));
-}
-
-void print_megaphone(char **av)
-{
-	for(int i = 1; av[i] ; i++)
-	{
-		ft_putstr(av[i]);
-		if(av[i + 1] != NULL)
-			std::cout << ' ';
-	}
-	std::cout << std::endl;
-}
-
 int main(int ac, char **av)
 {
 	if(ac == 1)
-		std::cout << "* LOUD AND UNBERABLE FEEDBACK NOISE *" << std::endl;
-	else
-		print_megaphone(av);
-	return (EXIT_SUCCESS);
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	for(int i = 1; av[i]; i++)
+	{
+		for(int j = 0; av[i][j]; j++)
+			std::cout << char(toupper(av[i][j]));
+		(av[i + 1]) ? (std::cout << " ") : (std::cout << std::endl);
+	}
+	return EXIT_SUCCESS;
 }
