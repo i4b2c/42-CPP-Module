ef211e6a02a9183ad7ef69328da569723c37b6de
icaldas <traiguuhh@gmail.com>
2024-02-19 17:57:17 +0000

    done

---
+++ CPP-Module-07/ex00/whatever.hpp
@@ -0,0 +1,31 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+template<typename T>
+void swap(T & a, T & b)
+{
+	T temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+template<typename T>
+T min(T a, T b)
+{
+	if(a < b)
+		return a;
+	return b;
+}
+
+template<typename T>
+T max(T a, T b)
+{
+	if(a > b)
+		return a;
+	return b;
+}
+
+#endif
