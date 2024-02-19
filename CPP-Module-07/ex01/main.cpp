e6737646577f905e61dc93bcbb87b7aad1d8c4e5
icaldas <traiguuhh@gmail.com>
2024-02-19 19:33:10 +0000

    not done yet , need to organize

--- CPP-Module-07/ex01/main.cpp
+++ CPP-Module-07/ex01/main.cpp
@@ -29,24 +29,3 @@ int main(void)
 	print_array(num,5,functionAddOne);
 	print_array(teste,3,functionAddOne);
 }
-
-// #include <iostream>
-
-// // Função de exemplo que recebe um parâmetro de função e a aplica
-// template<typename T>
-// void aplicarFuncao(T valor, void (*funcao)(T)) {
-//     funcao(valor);
-// }
-
-// // Função de exemplo que imprime o valor passado para ela
-// template<typename T>
-// void imprimirValor(T valor) {
-//     std::cout << "Valor: " << valor << std::endl;
-// }
-
-// int main() {
-//     // Usando a função de aplicação com a função de impressão
-//     aplicarFuncao(5.5, imprimirValor);
-
-//     return 0;
-// }
