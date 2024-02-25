fede2a7e507130daf34e6f9da061c194a60ed2b8
icaldas <traiguuhh@gmail.com>
2024-02-25 18:01:31 +0000

    almost done

--- CPP-Module-08/ex02/MutantStack.hpp
+++ CPP-Module-08/ex02/MutantStack.hpp
@@ -3,19 +3,38 @@
 
 #include <stack>
 
-#ifndef DEBUG_MODE
-# define DEBUG_MODE 1
-#endif // DEBUG_MODE
+// #ifndef DEBUG_MODE
+// # define DEBUG_MODE 1
+// #endif // DEBUG_MODE
 
 template <typename T>
 class MutantStack: public std::stack<T>
 {
 	public:
 		MutantStack();
+		~MutantStack();
 		
-		typename std::stack<T>::container_type::iterator begin();
-		typename std::stack<T>::container_type::iterator end();
+		typedef typename std::stack<T>::container_type Container;
 
+		typedef typename Container::iterator iterator;
+
+		iterator begin();
+		iterator end();
+
+		typedef typename Container::reverse_iterator reverse_iterator;
+
+		reverse_iterator rbegin();
+		reverse_iterator rend();
+
+		typedef typename Container::const_iterator const_iterator;
+
+		const_iterator cbegin();
+		const_iterator cend();
+
+		typedef typename Container::const_reverse_iterator const_reverse_iterator;
+
+		const_reverse_iterator crbegin();
+		const_reverse_iterator crend();
 };
 
 template<typename T>
@@ -24,6 +43,14 @@ MutantStack<T>::MutantStack(void)
 	#ifdef DEBUG_MODE
 	std::cout << "MutantStack constructor called" << std::endl;
 	#endif	
+}
+
+template<typename T>
+MutantStack<T>::~MutantStack(void)
+{
+	#ifdef DEBUG_MODE
+	std::cout << "MutantStack destructor called" << std::endl;
+	#endif
 }
 
 template <typename T>
@@ -36,6 +63,42 @@ template <typename T>
 typename std::stack<T>::container_type::iterator MutantStack<T>::end()
 {
 	return this->c.end();
+}
+
+template <typename T>
+typename std::stack<T>::container_type::reverse_iterator MutantStack<T>::rbegin()
+{
+	return this->c.rbegin();
+}
+
+template <typename T>
+typename std::stack<T>::container_type::reverse_iterator MutantStack<T>::rend()
+{
+	return this->c.rend();
+}
+
+template <typename T>
+typename std::stack<T>::container_type::const_iterator MutantStack<T>::cbegin()
+{
+	return MutantStack<T>::c.begin();
+}
+
+template <typename T>
+typename std::stack<T>::container_type::const_iterator MutantStack<T>::cend()
+{
+	return MutantStack<T>::c.end();
+}
+
+template <typename T>
+typename std::stack<T>::container_type::const_reverse_iterator MutantStack<T>::crbegin()
+{
+	return MutantStack<T>::c.rbegin();
+}
+
+template <typename T>
+typename std::stack<T>::container_type::const_reverse_iterator MutantStack<T>::crend()
+{
+	return MutantStack<T>::c.rend();
 }
 
 #endif //MUTANTSTACK_HPP
--- CPP-Module-08/ex02/main.cpp
+++ CPP-Module-08/ex02/main.cpp
@@ -4,18 +4,83 @@
 
 int main(void)
 {
-	MutantStack<int> teste;
-	MutantStack<int>::stack<int>::container_type::iterator it,itn;
-	teste.push(10);
-	teste.push(20);
-	teste.push(30);
-	teste.push(40);
-	it = teste.begin();
-	itn = teste.end();
-	while(it != itn)
 	{
-		std::cout << *it << std::endl;
-		it++;
+		std::cout << "Testing with normal iterators" << std::endl << std::endl;
+		MutantStack<int> teste;
+		MutantStack<int>::stack<int>::container_type::iterator it,itn;
+
+		teste.push(10);
+		teste.push(20);
+		teste.push(30);
+		teste.push(40);
+
+		it = teste.begin();
+		itn = teste.end();
+
+		while(it != itn)
+		{
+			std::cout << *it << std::endl;
+			it++;
+		}
+		std::cout << std::endl;
+	}
+	{
+		std::cout << "Testing with reverse iterators" << std::endl << std::endl;
+		MutantStack<int> teste;
+		MutantStack<int>::stack<int>::container_type::reverse_iterator rit, ritn;
+
+		teste.push(10);
+		teste.push(20);
+		teste.push(30);
+		teste.push(40);
+
+		rit = teste.rbegin();
+		ritn = teste.rend();
+
+		while(rit != ritn)
+		{
+			std::cout << *rit << std::endl;
+			rit++;
+		}
+		std::cout << std::endl;
+	}
+	{
+		std::cout << "Testing with const iterators" << std::endl << std::endl;
+		MutantStack<int> teste;
+
+
+		teste.push(10);
+		teste.push(20);
+		teste.push(30);
+		teste.push(40);
+
+		MutantStack<int>::stack<int>::container_type::const_iterator cit = teste.cbegin();
+		MutantStack<int>::stack<int>::container_type::const_iterator citn = teste.cend();
+
+		while(cit != citn)
+		{
+			std::cout << *cit << std::endl;
+			cit++;
+		}
+		std::cout << std::endl;
+	}
+	{
+		std::cout << "Testing with const reverse iterators" << std::endl << std::endl;
+		MutantStack<int> teste;
+
+		teste.push(10);
+		teste.push(20);
+		teste.push(30);
+		teste.push(40);
+
+		MutantStack<int>::stack<int>::container_type::const_reverse_iterator crit = teste.crbegin();
+		MutantStack<int>::stack<int>::container_type::const_reverse_iterator critn = teste.crend();
+
+		while(crit != critn)
+		{
+			std::cout << *crit << std::endl;
+			crit++;
+		}
 	}
 	return EXIT_SUCCESS;
 }
