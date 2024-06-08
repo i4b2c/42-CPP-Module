d9fd8856cdc46b5c7882ce8a233f5f58d042f8b4
icaldas <traiguuhh@gmail.com>
2024-06-08 13:55:59 +0100

    main improved and finish module 02

--- CPP-Module-02/ex03/main.cpp
+++ CPP-Module-02/ex03/main.cpp
@@ -5,28 +5,31 @@ int main( void )
 {
 	/*
 	GeoGebra para analisar os triangulos melhor
+	https://www.geogebra.org/calculator
 	*/
 	{
 		Point a;
 		Point const b(4,0);
-		Point const c(2,3);
+		Point const c(2,2);
 		Point const point_to_test(1,1);
 
-		if (bsp(a,b,c,point_to_test) == true)
-			std::cout << "true" << std::endl;
-		else
-			std::cout << "false" << std::endl;
+		std::cout << (bsp(a,b,c,point_to_test) ? "Inside" : "Outside") << std::endl;
 	}
 	{
-		Point const a(-1,0);
-		Point const b(1.4082644628099, 1.9559504132231);
-		Point const c(1.7719008264463, -0.8043801652893);
-		Point const point_to_test(0.5003979185797, 1.2507162534435);
-		if (bsp(a,b,c,point_to_test) == true)
-			std::cout << "true" << std::endl;
-		else
-			std::cout << "false" << std::endl;
+		Point a;
+		Point const b(4,0);
+		Point const c(2,2);
+		Point const point_to_test(1,1.1f);
 
+		std::cout << (bsp(a,b,c,point_to_test) ? "Inside" : "Outside") << std::endl;
+	}
+	{
+		Point a;
+		Point const b(4,0);
+		Point const c(2,2);
+		Point const point_to_test(1.1f,1);
+
+		std::cout << (bsp(a,b,c,point_to_test) ? "Inside" : "Outside") << std::endl;
 	}
 	return 0;
 }
